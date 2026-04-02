edc5b04845d30e119c73e06ca40c0c8276a10f82
GrCOTE7 <grcote7@gmail.com>
2026-04-02 19:57:55 +0200

    ini

---
+++ src/main_template.py
@@ -0,0 +1,17 @@
+import flet as ft
+import tools.screen_utils as screen_utils
+import datetime
+
+name = "Ready"
+
+def main(page: ft.Page):
+
+    screen_utils.configure_window(page)
+    page.theme_mode = ft.ThemeMode.DARK  # Comment to light
+    page.title = "Flet Doc officielle | "+ name
+
+    page.add(ft.Text("Ready.", size=28))
+    print("oki")
+
+
+ft.run(main)

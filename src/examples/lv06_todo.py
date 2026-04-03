9bcea79392cdac641bbb4418fa6589ed3c55c6a8
GrCOTE7 <grcote7@gmail.com>
2026-04-03 15:00:06 +0200

    Set icons list

--- src/examples/lv06_todo.py
+++ src/examples/lv06_todo.py
@@ -1,37 +1,87 @@
-from dataclasses import field
+# from dataclasses import field
+
+from ctypes import alignment
 
 import flet as ft
 
+# @ft.control
+# class todo(ft.Container):
 
-@ft.control
-class todo(ft.Container):
-    def init(self):
-        self._values: dict[str, int] = {"x": 0}
+# def init(self):
+#     self._values: dict[str, int] = {"x": 0}
 
-        self.bgcolor = "#333333"
-        self.width = 350
-        self.height = 200
-        self.border_radius = ft.BorderRadius.all(12)
-        self.padding = 20
-        self.content = ft.Column(
-            controls=[
-                ft.Row(
-                    controls=[
-                        ft.Text(value="Todo List", color=ft.Colors.WHITE, size=20),
-                    ],
-                    alignment=ft.MainAxisAlignment.CENTER,
-                )
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-        )
+#     self.bgcolor = "#333333"
+#     self.width = 350
+#     self.height = 200
+#     self.border_radius = ft.BorderRadius.all(12)
+#     self.padding = 20
+#     self.content = ft.Column(
+#         controls=[
+#             ft.Row(
+#                 controls=[
+#                     ft.Text(value="Todo List", color=ft.Colors.WHITE, size=20),
+#                 ],
+#                 alignment=ft.MainAxisAlignment.CENTER,
+#             )
+#         ],
+#         alignment=ft.MainAxisAlignment.CENTER,
+#         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+#     )
+
+# pass
 
 
 def todo_list(page: ft.Page):
     page.title = "Todo List"
+    page.bgcolor = ft.Colors.BLACK
 
-    todo_instance = todo()
-    page.add(todo_instance)
+    # print(dir(ft.Icons))
+    icons_ids = dir(ft.Icons)
+
+    print(len(icons_ids))
+    # print(*icons_ids[:3], sep=" ")
+    print (ft.Icons.ABC)
+    # print (ft.Icons.ABC_ROUNDED)
+    page.add(ft.IconButton(ft.Icons.ABC, on_click=lambda e: print("ABC clicked")))
+
+    # --- Titre centré ---
+    title = ft.Row(
+        controls=[ft.Text("Todo List", color=ft.Colors.WHITE, size=32)],
+        alignment=ft.MainAxisAlignment.CENTER,
+    )
+    page.add(title)
+
+    # --- Champ de saisie ---
+    new_task = ft.TextField(hint_text="What needs to be done?", expand=True)
+
+    # --- Action bouton ---
+    def add_clicked(e):
+        if new_task.value.strip() != "":
+            page.add(ft.Checkbox(label=new_task.value))
+            new_task.value = ""
+            page.update()
+
+    add_button = ft.FloatingActionButton(icon=ft.Icons.ADD_CIRCLE, on_click=add_clicked)
+
+    # --- Ligne d'ajout ---
+    page.add(
+        ft.Row(controls=[new_task, add_button], alignment=ft.MainAxisAlignment.CENTER)
+    )
+
+    page.update()
+
+    # page.add(ft.Text(value="New Todo Item", color=ft.Colors.WHITE, size=20))
+
+    # def add_clicked(e):
+    #     page.add(ft.Checkbox(label=new_task.value))
+    #     new_task.value = ""
+
+    # # todo_instance = todo()
+    # # page.add(todo_instance)
+    # new_task = ft.TextField(hint_text="Whats needs to be done?")
+    # page.update()
+
+    # page.add(new_task, ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_clicked))
 
 
 if __name__ == "__main__":
--- src/main.py
+++ src/main.py
@@ -11,8 +11,11 @@ def main(page: ft.Page):
     # from examples.lv05_calc_ui_reusable import calc as calc
     # calc(page)
 
+    # from devs.lv01_icons_list import icons_list as icons_list
+    # icons_list(page) # 3 versions dispos
+    
     # # ❌ Finir game NbreX
-    # from devs.lv01_nbre_x import game as game
+    # from devs.lv02_nbre_x import game as game
     # game(page)
     
     from examples.lv06_todo import todo_list as todo

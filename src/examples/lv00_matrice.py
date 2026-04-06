77576f6a4867c9b255702fecb6269196bbf610fd
GrCOTE7 <grcote7@gmail.com>
2026-04-06 10:04:06 +0200

    up Matrice POO

--- src/examples/lv00_matrice.py
+++ src/examples/lv00_matrice.py
@@ -1,6 +1,20 @@
 import flet as ft
 
 
-def main(page: ft.Page):
+@ft.control
+class Matrice(ft.Column):
+    def init(self):
+        self.title = ft.Text("Matrice Ready.", size=24)
+        self.controls = [self.title]
 
-    page.add(ft.Text("Ready → Matrice.", size=18))
+
+def main(page: ft.Page):
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.bgcolor = "#151515"
+
+    page.add(Matrice())
+
+
+if __name__ == "__main__":
+
+    ft.run(main)
